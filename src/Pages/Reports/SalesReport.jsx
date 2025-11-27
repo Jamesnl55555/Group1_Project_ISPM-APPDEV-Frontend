7ff99f62f9989e42253446181394384a9e70f961
Jamesnl55555 <jamesnl55555@gmail.com>
2025-11-27 16:33:26 +0800

    fixed capital

--- src/Pages/Reports/SalesReport.jsx
+++ src/Pages/Reports/SalesReport.jsx
@@ -6,19 +6,21 @@ import { useNavigate } from 'react-router-dom';
 export default function CreateReport() {
   const [transactions, setTransactions] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+  const [capital, setCapital] = useState([]);
   useEffect(() => {
-    const fetchTransactions = async () => {
+    const fetchDatas = async () => {
       try {
         const response = await axios.get('/api/fetchtransactions');
         setTransactions(response.data.transactions);
+        const response2 = await axios.get('/api/fetchcapital');
+        setTransactions(response2.data.transactions);
       } catch (err) {
-        console.error('Failed to fetch transactions:', err);
+        console.error('Failed to fetch datas:', err);
       } finally {
         setLoading(false);
       }
     };
-    fetchTransactions();
+    fetchDatas();
   }, []);
   
   const navigate = useNavigate();
@@ -155,7 +157,7 @@ export default function CreateReport() {
             capital.map((item, index) => (
               <tr key={index} className="hover:bg-[#f9f5f0] text-center">
                 <td className="border-b border-gray-300 px-6 py-2">#{item.id}</td>
-                <td className="border-b border-gray-300 px-6 py-2">{item.date}</td>
+                <td className="border-b border-gray-300 px-6 py-2">{new Date(item.created_at).toLocaleString()}</td>
                 <td className="border-b border-gray-300 px-6 py-2">₱ {item.amount}</td>
                 <td className="border-b border-gray-300 px-4 py-2 text-right">→</td>
               </tr>
