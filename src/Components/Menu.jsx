b6acf29b4ff3b10b1d0f101f9d68b2ffd3e95599
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-02 06:38:27 +0800

    modified Menu

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -4,12 +4,12 @@ export default function Menu({ toggleMenu }) {
       onClick={toggleMenu}
       title="Menu"
       aria-label="Toggle menu"
-      className="cursor-pointer p-2 rounded bg-transparent border-none relative z-50"
+      className="cursor-pointer p-2 rounded bg-transparent border-none relative z-[50]"
     >
-      <div className="flex flex-col justify-center items-center gap-1">
-        <span className="block w-5 h-0.5 bg-gray-700 rounded-sm"></span>
-        <span className="block w-5 h-0.5 bg-gray-700 rounded-sm"></span>
-        <span className="block w-5 h-0.5 bg-gray-700 rounded-sm"></span>
+      <div className="flex flex-col justify-center items-center gap-[4px]">
+        <span className="block w-[20px] h-[2px] bg-[#374151] rounded"></span>
+        <span className="block w-[20px] h-[2px] bg-[#374151] rounded"></span>
+        <span className="block w-[20px] h-[2px] bg-[#374151] rounded"></span>
       </div>
     </button>
   );
