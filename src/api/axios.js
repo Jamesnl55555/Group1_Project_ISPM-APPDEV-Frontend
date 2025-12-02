849a73a548bf9b72f02df71768975e7bb8520c02
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-03 05:39:15 +0800

    Fixing

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,27 +2,11 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, 
+  withCredentials: true, // send cookies
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
 });
 
-const token = localStorage.getItem('auth_token') || sessionStorage.getItem('auth_token');
-if (token) {
-  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
-
-// Optional: intercept requests to always attach token (if it changes dynamically)
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('auth_token') || sessionStorage.getItem('auth_token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
+// Remove token logic entirely for session-based auth
 export default axiosInstance;
