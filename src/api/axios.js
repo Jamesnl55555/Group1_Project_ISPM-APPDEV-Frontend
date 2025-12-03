a6c030bd9e7c584c5ced498b3b2c0b6db0761bac
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-04 06:28:22 +0800

    fixing

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,12 +4,15 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, 
   headers: {
-    "Content-Type": "application/json",
     "Accept": "application/json",
     "X-Requested-With": "XMLHttpRequest",
   },
 });
-
-axiosInstance.defaults.withCredentials = true;
-
+axiosInstance.interceptors.request.use((config) => {
+  const match = document.cookie.match(/XSRF-TOKEN=([^;]+)/);
+  if (match) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(match[1]);
+  }
+  return config;
+});
 export default axiosInstance;
