9173c4714a103bee560e0a57ac4d1f9aecf51eff
Jamesnl55555 <jamesnl55555@gmail.com>
2025-12-08 13:46:12 +0800

    fixing

--- src/Pages/SalesReports/GenerateSalesReportMonthly.jsx
+++ src/Pages/SalesReports/GenerateSalesReportMonthly.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import axios from "axios";
+import axios from "@/api/axios";
 
 export default function GenerateSalesReportMonthly() {
   const [monthlySales, setMonthlySales] = useState([]);
